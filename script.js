547d5e27692e84be9cc5772880b0f920205a3bc4
Mateus Victor <mateusvictorct@gmail.com>
2024-08-10 12:06:45 -0300

    add a função de add tarefas

---
+++ script.js
@@ -0,0 +1,10 @@
+function addTarefa(){
+    let input = document.querySelector('#input-tarefa');
+
+    if(!input.value){
+        alert('ATENÇÃO! Insira a tarefa no campo.');
+    }else{
+
+        // verificar se a tarefa ja foi inserida
+    }
+}
